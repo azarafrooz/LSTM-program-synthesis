d432a25fd42c0ceac444824285ff0eab7da04f11
mahdi azarafrooz <mahdiazarafrooz@mahdis-MacBook-Pro.local>
2018-11-01 23:25:49 -0700

    cleaning repo a bit

--- test/adversarial_sketch/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns adversarial-sketch.core-test
-  (:require [clojure.test :refer :all]
-            [adversarial-sketch.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
